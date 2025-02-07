7ce1cc85b365e0d756d84fbd08a7192669b1d94a
Nanfaice <ice.nanfa@gmail.com>
2025-02-07 10:14:23 +0700

    Add files via upload

---
+++ QuizzApp-main/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/start_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      home: StartScreen(), // เรียก StartScreen โดยตรง
+    )
+  );
+}
